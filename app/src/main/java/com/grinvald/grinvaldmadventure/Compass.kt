4129022da4edc7d72b3a65379d5b5a61e062016c
student <student@Radiotech.local>
2021-01-19 19:58:43 +0500

    Compass screen

--- app/src/main/java/com/grinvald/grinvaldmadventure/Compass.kt
+++ app/src/main/java/com/grinvald/grinvaldmadventure/Compass.kt
@@ -1,5 +1,6 @@
 package com.grinvald.grinvaldmadventure
 
+
 import android.content.Context
 import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
@@ -8,21 +9,20 @@ import android.hardware.Sensor.TYPE_MAGNETIC_FIELD
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_GAME
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation.RELATIVE_TO_SELF
+import android.view.animation.RotateAnimation
 import android.widget.ImageView
-import androidx.core.graphics.rotationMatrix
-import java.lang.Math.round
+import androidx.core.content.ContextCompat.getSystemService
+import java.lang.Math.toDegrees
 
 
 class Compass : Fragment(), SensorEventListener {
-
-    lateinit var iv_arrow : ImageView
-    var sCompass : Sensor? = null
 
     lateinit var sensorManager: SensorManager
     lateinit var accelerometer: Sensor
@@ -34,18 +34,23 @@ class Compass : Fragment(), SensorEventListener {
     var lastAccelerometerSet = false
     var lastMagnetometerSet = false
 
+    lateinit var mContext : Context
+
+    lateinit var image : ImageView
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val v = inflater.inflate(R.layout.fragment_compass, container, false)
 
-        iv_arrow = v.findViewById(R.id.iv_arrow)
-
-        sensorManager = requireContext().getSystemService(SENSOR_SERVICE) as SensorManager
-        sCompass = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-        sensorManager.registerListener(this, sCompass, SensorManager.SENSOR_DELAY_NORMAL)
-
+        image = v.findViewById(R.id.iv_arrow)
+        sensorManager = mContext.getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER)
         magnetometer = sensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD)
 
@@ -56,24 +61,54 @@ class Compass : Fragment(), SensorEventListener {
     override fun onResume() {
         super.onResume()
 
-
+        sensorManager.registerListener(this, accelerometer, SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetometer, SENSOR_DELAY_GAME)
     }
 
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        val value = event!!.values[0]
-
-        SensorManager.getRotationMatrix(FloatArray(9), null, FloatArray(3), FloatArray(9))
-
-        val orientation = SensorManager.getOrientation(FloatArray(9), FloatArray(3))
-        val degrees = (Math.toDegrees(orientation.get(0).toDouble()) + 360.0) % 360.0
-        val angle = round(degrees * 100) / 100
-
-        iv_arrow.rotation = angle.toFloat()
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this, accelerometer)
+        sensorManager.unregisterListener(this, magnetometer)
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        Log.d("DEBUG", "asd")
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor === accelerometer) {
+            lowPass(event.values, lastAccelerometer)
+            lastAccelerometerSet = true
+        } else if (event.sensor === magnetometer) {
+            lowPass(event.values, lastMagnetometer)
+            lastMagnetometerSet = true
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            val r = FloatArray(9)
+            if (SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(r, orientation)
+                val degree = (toDegrees(orientation[0].toDouble()) + 360).toFloat() % 360
+
+                val rotateAnimation = RotateAnimation(
+                        currentDegree,
+                        -degree,
+                        RELATIVE_TO_SELF, 0.5f,
+                        RELATIVE_TO_SELF, 0.5f)
+                rotateAnimation.duration = 1000
+                rotateAnimation.fillAfter = true
+
+                image.startAnimation(rotateAnimation)
+                currentDegree = -degree
+            }
+        }
+    }
+
+    fun lowPass(input: FloatArray, output: FloatArray) {
+        val alpha = 0.05f
+
+        for (i in input.indices) {
+            output[i] = output[i] + alpha * (input[i] - output[i])
+        }
     }
 
 }
